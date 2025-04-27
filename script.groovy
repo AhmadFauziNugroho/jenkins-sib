002a9ab5575637cc011fcd0e2b45768b7d33c1c2
AhmadFauziNugroho <114461374+AhmadFauziNugroho@users.noreply.github.com>
2025-04-27 15:54:32 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t azeshion21/demo-app:jma-2.0 .'
+        sh 'docker build -t AhmadFauziNugroho/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push azeshion21/demo-app:jma-2.0'
+        sh 'docker push AhmadFauziNugroho/demo-app:jma-2.0'
     }
 } 
 
